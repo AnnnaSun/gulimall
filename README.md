bc5f5c68a45a2aab947e49b6893a381e1d379e1c
AnnaSun <annnasun@outlook.com>
2024-02-22 00:01:06 +0800

    initial project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gulimall
 bilibili: BV1np4y1C7Yf 分布式练手项目
+
+tips:后期需要忽略commit的文件可以在.ignore里配置
---
+++ gulimall-member/src/main/java/com/community/gulimall/product/member/Main.java
@@ -0,0 +1,7 @@
+package com.community.gulimall.product.member;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
