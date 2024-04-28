fc1cbda52770076e8158c90284a324c70364565a
AnnaSun <annnasun@outlook.com>
2024-04-28 22:27:07 +0800

    add oss dependency without secret

--- gulimall-product/src/test/java/com/community/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/community/gulimall/product/GulimallProductApplicationTests.java
@@ -1,16 +1,29 @@
 package com.community.gulimall.product;
 
+import com.aliyun.oss.*;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
 import com.community.gulimall.product.entity.BrandEntity;
 import com.community.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+/**
+ * 引入oss包。配置accesskey
+ */
 @SpringBootTest()
 public class GulimallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+
+    @Autowired
+    OSSClient ossClient;
 
     @Test
     void contextLoads() {
@@ -23,6 +36,45 @@ public class GulimallProductApplicationTests {
         brandService.updateById(brandEntity);
         System.out.println("save successfully");
 
+    }
+
+    @Test
+    public void testUpload() {
+        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+//        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "gulimall-ysun";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "testimage/test2.jpg";
+        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        String filePath= "C:\\Users\\Annna\\Pictures\\Camera Roll\\微信图片_20210520013755.jpg";
+        try {
+            InputStream inputStream = new FileInputStream(filePath);
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, inputStream);
+            // 创建PutObject请求。
+            PutObjectResult result = ossClient.putObject(putObjectRequest);
+            System.out.println("upload successfully");
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
     }
 
 }
